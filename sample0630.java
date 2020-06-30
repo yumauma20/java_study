b16ea3dcf6e23cc6b71dd03a7d5a868b495fe557
yumauma20 <71yuma10@gmail.com>
2020-06-30 16:11:15 +0900

    変数で大きさを変更できるように変更

--- sample0630.java
+++ sample0630.java
@@ -1,7 +1,9 @@
 public class sample0630 {
   public static void main(String[] args) {
-    for(int i=1; i<=95; i++) {
-      for(int j=1; j<=95-i; j++) {
+    int x;
+    x = 80;
+    for(int i=1; i<=x; i++) {
+      for(int j=1; j<=x-i; j++) {
         System.out.print(" ");
       }
       for(int m=1; m<=i*2-1; m++) {
