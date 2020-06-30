91ce51b6ee5e9b1de6de73789f82bd8554ce0c5c
yumauma20 <71yuma10@gmail.com>
2020-06-30 15:50:51 +0900

    5×5プログラム

--- sample0630.java
+++ sample0630.java
@@ -1,8 +1,8 @@
 public class sample0630 {
   public static void main(String[] args) {
-    for(int i=1; i<=9; i++) {
-      for (int j=1; j<=9; j++) {
-        System.out.print((i*j)+"\t");
+    for(int i=0; i<5; i++) {
+      for (int j=1; j<=5; j++) {
+        System.out.print("*");
       }
       System.out.println();
     }
