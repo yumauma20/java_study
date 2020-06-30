4cd93350e264426419c21ad8fa9cf9d390e0928f
yumauma20 <71yuma10@gmail.com>
2020-06-30 15:58:31 +0900

    逆三角形

--- sample0630.java
+++ sample0630.java
@@ -1,7 +1,10 @@
 public class sample0630 {
   public static void main(String[] args) {
-    for(int i=0; i<5; i++) {
-      for (int j=1; j<=5; j++) {
+    for(int i=1; i<=5; i++) {
+      for(int j=1; j<=5-i; j++) {
+        System.out.print(" ");
+      }
+      for(int m=1; m<=i; m++) {
         System.out.print("*");
       }
       System.out.println();
