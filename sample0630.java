bfa360fc8cd4d64550a458e4c2282d432adc2ac2
yumauma20 <71yuma10@gmail.com>
2020-06-30 16:03:25 +0900

    めっちゃでかい三角形

--- sample0630.java
+++ sample0630.java
@@ -1,10 +1,10 @@
 public class sample0630 {
   public static void main(String[] args) {
-    for(int i=1; i<=5; i++) {
-      for(int j=1; j<=5-i; j++) {
+    for(int i=1; i<=95; i++) {
+      for(int j=1; j<=95-i; j++) {
         System.out.print(" ");
       }
-      for(int m=1; m<=i; m++) {
+      for(int m=1; m<=i*2-1; m++) {
         System.out.print("*");
       }
       System.out.println();
